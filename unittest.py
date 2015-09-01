a0aadf53d72079eeb348a0959a3bd81c5accb409
Swoorup Joshi <swoorupj@gmail.com>
2015-09-02 02:58:41 +0800

    Mirno

--- unittest.py
+++ unittest.py
@@ -1,9 +1,7 @@
 ﻿__author__ = 'Swoorup'
-from gta import sapaths
+import sapaths
 import imp
 imp.reload(sapaths)
 
-import main
-main.main()
-#paths = sapaths.SAPaths()
-#paths.load_nodes_from_directory(r"E:\paths-visualizer\Vanilla\Compiled\OpenVice")
+paths = sapaths.SAPaths()
+paths.load_nodes_from_directory(r"E:\Output\LC")
