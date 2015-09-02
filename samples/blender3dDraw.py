c8c626d2125b1cb08ec6e8bf7431952ef76cb427
Swoorup Joshi <swoorupj@gmail.com>
2015-09-02 20:01:55 +0800

    Working edge direction

--- samples/blender3dDraw.py
+++ samples/blender3dDraw.py
@@ -1,30 +1,94 @@
 import bpy
 import bgl
 import blf
+import bmesh
+from mathutils import *
+from math import *
 
+def draw_arrow_head(self, context, vecFrom, vecTo):
+    ob = context.object
+    if ob is None:
+        return
+    
+    arrow = [
+        [-1,-1],
+        [0,0],
+        [1,-1],
+    ]
+    
+    middle = (Vector(vecTo) + Vector(vecFrom)) / 2.0
+    
+    v = Vector(vecTo) - Vector(vecFrom)
+    v.normalize()
+    
+    vPerp1 = Vector((-v.y, v.x, 0.0))
+    vPerp2 = Vector((v.y, -v.x, 0.0))
+    
+    v1 = (vPerp1 - v).normalized()
+    v2 = (vPerp2 - v).normalized()
+    
+    bgl.glBegin(bgl.GL_LINE_STRIP)
+    bgl.glVertex3f(*(middle + v1))
+    bgl.glVertex3f(*(middle))
+    bgl.glVertex3f(*(middle + v2))
+    bgl.glEnd()
+    
+    """
+    direction = Vector(vecTo) - Vector(vecFrom)
+    angle = direction.angle(Vector((1,0,0 )))
+    
+    # form a 2d rotation matrix
+    mat = Matrix()
+    mat.rotate((0,0,angle))
+    
+    # middle point
+    
+    
+    bgl.glBegin(bgl.GL_LINE_STRIP)
+    for v in arrow:
+        xy = mat * Vector(v).to_3d()
+        newPos = xy + middle
+        bgl.glVertex3f(*newPos)
+    bgl.glEnd()
+    """
 
+def draw_edge_direction(self, context):
+    ob = context.object
+    if ob is None:
+        return
+    
+    if bpy.context.active_object.mode != 'EDIT':
+        return
+        
+    obj = context.edit_object
+    me = obj.data
+    
+    bm = bmesh.from_edit_mesh(me)
+    
+    for e in bm.edges:
+        draw_arrow_head(self, context, e.verts[0].co, e.verts[1].co)
+    
+    
 def draw_callback_px(self, context):
 
     ob = context.object
     if ob is None:
         return
 
-    font_id = 0  # XXX, need to find out how best to get this.
-
-    # draw some text
-    blf.position(font_id, 15, 30, 0)
-    blf.size(font_id, 20, 72)
-    blf.draw(font_id, ob.name)
-
     # 50% alpha, 2 pixel width line
     bgl.glEnable(bgl.GL_BLEND)
     bgl.glColor4f(1.0, 1.0, 1.0, 0.5)
     bgl.glLineWidth(2)
-
+    
+    #draw_arrow_head(self, context, [1,1,0], [0,10,0])
+    draw_edge_direction(self, context)
+    
+    """
     bgl.glBegin(bgl.GL_LINE_STRIP)
     bgl.glVertex3f(*ob.matrix_world.translation)
     bgl.glVertex3f(*context.scene.cursor_location)
     bgl.glEnd()
+    """
 
     # restore opengl defaults
     bgl.glLineWidth(1)
